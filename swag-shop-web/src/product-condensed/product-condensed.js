95a8f09c10d997cbaae4ee8ffda2bcdd72c208fb
cybnom <uriel.fiori@gmail.com>
2019-04-29 20:10:12 -0300

    add and remove wishlist items

--- swag-shop-web/src/product-condensed/product-condensed.js
+++ swag-shop-web/src/product-condensed/product-condensed.js
@@ -3,12 +3,27 @@ import React, {
 } from 'react';
 
 import './product-condensed.css';
+import DataService from '../services/data-service';
+
+let ds = new DataService();
 
 class ProductCondensed extends Component {
+
+  constructor(props) {
+    super(props);
+
+    //binds
+    this.removeProduct = this.removeProduct.bind(this);
+  }
+
+  removeProduct = () => {
+    ds.removeWishListItem(this.props.product);
+  }
+
   render () {
     return (
       <li className="list-group-item pc-condensed">
-        <a className="btn btn-outline-danger">X</a>
+        <a className="btn btn-outline-danger" onClick={() => this.removeProduct()}>X</a>
         <p>{this.props.product.title} | <b>${this.props.product.price}</b></p>
       </li>
 
--- swag-shop-web/src/product/product.js
+++ swag-shop-web/src/product/product.js
@@ -4,30 +4,60 @@ import React, {
 
 import './product.css';
 import DataService from '../services/data-service';
+import NotificationService, {NOTIF_WISHLIST_CHANGED} from '../services/notification-service';
 
 let ds = new DataService();
+let ns = new NotificationService();
 
 class Product extends Component {
 
   constructor(props) {
     super(props);
 
+    this.state = {onWishList: ds.itemOnWishList()}
+
     //bind functions
     this.onButtonClicked = this.onButtonClicked.bind(this);
+    this.onWishListChanged = this.onWishListChanged.bind(this);
+  }
+
+  componentDidMount() {
+    ns.addObserver(NOTIF_WISHLIST_CHANGED, this, this.onWishListChanged);
+  }
+
+  componentWillUnmount() {
+    ns.removeObserver(this, NOTIF_WISHLIST_CHANGED);
+  }
+
+  onWishListChanged(newWishList) {
+    this.setState({onWishList: ds.itemOnWishList(this.props.product)})
   }
 
   onButtonClicked = () => {
-    ds.addWishListItem(this.props.product);
+    if (this.state.onWishList) {
+      ds.removeWishListItem(this.props.product)
+    } else {
+      ds.addWishListItem(this.props.product);
+    }
+
   }
 
   render () {
+    var btnClass;
+
+    if (this.state.onWishList) {
+      btnClass = "btn btn-danger";
+    } else {
+      btnClass = "btn btn-primary";
+    }
+
     return (
       <div className="card product">
         <img className="card-img-top" alt="Product" src={this.props.product.imgUrl}></img>
         <div className="card-block">
           <h4 className="card-title">{this.props.product.title}</h4>
           <p className="card-text">Price: ${this.props.product.price}</p>
-          <a href="#" onClick={() => this.onButtonClicked()} className="btn btn-primary">Add to Wishlist</a>
+          <a href="#" onClick={() => this.onButtonClicked()} className={btnClass}>{this.state.onWishList ? "Remove from Wishlist" : "Add to Cart"}</a>
         </div>
       </div>
     );
